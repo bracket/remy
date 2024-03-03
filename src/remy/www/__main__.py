ccc56d8cee01e941c56e3655095eb74c219e5851
Bracket <mcbracket@gmail.com>
2024-03-02 21:00:09 -0800

    * Add host option to stand-alone Flask server

--- src/remy/www/__main__.py
+++ src/remy/www/__main__.py
@@ -3,9 +3,10 @@ from .app import create_app
 
 @click.command
 @click.option('--cache', help='Location of Remy notecard cache.', required=True)
-def main(cache):
++@click.option('--host', help='Flask host to serve from', required=False)
+def main(cache, host=None):
     app = create_app(cache)
-    app.run()
+    app.run(host=host)
 
 
 if __name__ == '__main__':
